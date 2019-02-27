5b7c22fb9807d3c795c08137ee65c129f9c42ca9
alaynapuck28 <you@example.com>
2019-02-27 07:23:11 -0600

    checkpoint styling

---
+++ tic-tac-toe/tictactoe.js
@@ -0,0 +1,12 @@
+var gameMarker = "x"
+function changeMarkerToX(){
+    var gameMarker = "X"
+    console.log("The x button was clicked!")
+ };
+function changeMarkerToO(){
+    var gameMarker = "O"
+    console.log("The o button was clicked!")
+ };
+ function placeMark(){ 
+    console.log("The square was clicked!")
+ }
