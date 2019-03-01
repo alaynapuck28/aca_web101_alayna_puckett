ff01c8db24f41ab81ec0ba53b33c003a6c34db67
alaynapuck28 <you@example.com>
2019-03-01 07:33:24 -0600

    tic tac toe updates

--- tic-tac-toe/tictactoe.js
+++ tic-tac-toe/tictactoe.js
@@ -1,12 +1,30 @@
-var gameMarker = "x"
+var gameMarker = "X"
 function changeMarkerToX(){
-    var gameMarker = "X"
+     gameMarker = "X"
     console.log("The x button was clicked!")
  };
 function changeMarkerToO(){
-    var gameMarker = "O"
+     gameMarker = "O"
     console.log("The o button was clicked!")
  };
- function placeMark(){ 
+ function placeMark(myDiv){ 
     console.log("The square was clicked!")
+    document.getElementById(myDiv).innerHTML=gameMarker;
  }
+ 
+// function getWinner() {
+//  let box1 = document.getElementById ("box1"),
+//    box2 = document.getElementById ("box2"),
+//    box3 = document.getElementById ("box3"),
+//    box4 = document.getElementById ("box4"),
+//    box5 = document.getElementById ("box5"),
+//    box6 = document.getElementById ("box6"),
+//    box7 = document.getElementById ("box7"),
+//    box8 = document.getElementById ("box8"),
+//    box9 = document.getElementById ("box9");
+
+// if (box1.innerHTML !== "" && box1.innerHTML === box2.innerHTML && box1.innerHTML && box3.innerHTML)
+// console.log("win!");
+// else if (box4.innerHTML !== "" && box4.innerHTML === box5.innerHTML && box4.innerHTML && box6.innerHTML)
+// console.log("win!");
+// }
